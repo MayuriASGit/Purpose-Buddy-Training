602414b426bed928622ffe41bea465dd2cc2b221
MayuriASGit <sadavartemayuri12@gmail.com>
2024-09-10 20:11:49 +0530

    Create welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/welcome.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to Our Website</h1>
+    <p>This is the welcome page.</p>
+</body>
+</html>
